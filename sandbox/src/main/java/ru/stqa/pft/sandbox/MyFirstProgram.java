c9f46c22eaef8ef2769fe96e91319641cf2f5175
Home Windows <afonin_av@list.ru>
2020-04-14 17:59:34 +0300

    MyFirstTest

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hell, world!");
-	}
+    public static void main(String[] args) {
+        hello();
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    }
+
+    public static void hello() {
+        System.out.println("Hello, world!!!");
+    }
+
 
 }
